b8412f3bd7856cb0b6e84fb1058f0b8c43e2b675
Kyle Goyette <kdgoyette@gmail.com>
2021-06-04 12:52:53 -0700

    update project

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ parser.add_argument("--lr", type=float, default=0)
 parser.add_argument("--epochs", type=float, default=5)
 args = parser.parse_args()
 
-wandb.init(project="launchtest1")
+wandb.init(project="launch-demo")
 wandb.log({"lr": args.lr})
 wandb.log({"eopchs": args.epochs})
