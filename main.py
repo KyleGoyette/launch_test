ff37837749b5767d8c7902130a67503cbaedf4f2
Kyle Goyette <kdgoyette@gmail.com>
2021-06-04 11:26:01 -0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import wandb
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--lr", type=float, default=0)
+parser.add_argument("--epochs", type=float, default=5)
+args = parser.parse_args()
 
 wandb.init(project="launchtest1")
-wandb.log({"blah": 123})
+wandb.log({"lr": args.lr})
+wandb.log({"eopchs": args.epochs})
