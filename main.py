d8897c94fe3d54e6ad7524b38751c4b97f110867
Kyle Goyette <kdgoyette@gmail.com>
2021-06-13 23:58:52 -0700

    WIP

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ parser.add_argument("--epochs", type=float, default=5)
 args = parser.parse_args()
 
 wandb.init(project="launch-demo")
+wandb.run.config["val1"] = 10
+wandb.run.config["val2"] = 20
+wandb.run.config["lr"] = args.lr
+wandb.run.config["epochs"] = args.epochs
 wandb.log({"lr": args.lr})
 wandb.log({"eopchs": args.epochs})
