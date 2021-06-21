ef015c933dc72c1ce87c7d6e2aa326d7462e5404
Kyle Goyette <kdgoyette@gmail.com>
2021-06-21 15:58:54 -0700

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wandb
+import time
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -6,10 +7,12 @@ parser.add_argument("--lr", type=float, default=0)
 parser.add_argument("--epochs", type=float, default=5)
 args = parser.parse_args()
 
-wandb.init(project="launch-demo")
-wandb.run.config["val1"] = 10
-wandb.run.config["val2"] = 20
-wandb.run.config["lr"] = args.lr
-wandb.run.config["epochs"] = args.epochs
-wandb.log({"lr": args.lr})
-wandb.log({"eopchs": args.epochs})
+wandb.init(project="launch-demo10")
+for i in range(100):
+    time.sleep(0.5)
+    wandb.run.config["val1"] = 10
+    wandb.run.config["val2"] = 20
+    wandb.run.config["lr"] = args.lr
+    wandb.run.config["epochs"] = args.epochs
+    wandb.log({"lr": args.lr})
+    wandb.log({"eopchs": args.epochs})
