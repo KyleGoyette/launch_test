02d36928a2c67b594d63c47d32eadd55efee9983
Kyle Goyette <kdgoyette@gmail.com>
2021-06-23 15:45:08 -0700

    WIP

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--lr", type=float, default=0)
+parser.add_argument("--lr", type=float, default=0.1)
 parser.add_argument("--epochs", type=float, default=5)
-parser.add_argument("--flag", action="store_true")
+parser.add_argument("--accs", action="store_true")
 args = parser.parse_args()
 
-wandb.init(project="launch-demo10")
-wandb.run.config["val1"] = 10
-wandb.run.config["val2"] = 20
+wandb.init(project="launch-demo11")
 wandb.run.config["lr"] = args.lr
 wandb.run.config["flag"] = args.flag
 wandb.run.config["epochs"] = args.epochs
-for i in range(100):
-    #time.sleep(0.5)
-    wandb.log({"lr": wandb.config.lr})
-    wandb.log({"eopchs": args.epochs})
+for i in range(wandb.run.config.epochs):
+    wandb.log({"loss": 1/wandb.config.lr})
+    if wandb.run.config.accs:
+        wandb.log({"acc": 1 - 1/wandb.config.lr})
+
+    
