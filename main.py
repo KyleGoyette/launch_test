373d858d73cea4914ee2c0f591be31fd93dae75e
Kyle Goyette <kdgoyette@gmail.com>
2021-06-30 14:20:59 -0700

    update reqs

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import wandb
-import time
 import argparse
+import numpy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--lr", type=float, default=0.1)
 parser.add_argument("--epochs", type=int, default=5)
-parser.add_argument("--accs", action="store_true")
+parser.add_argument("--seed", type=int, default=10)
 args = parser.parse_args()
 
 wandb.init(project="launch-demo")
+wandb.run.config["nested"] = {"super_nested": "jerk"}
 wandb.run.config["lr"] = args.lr
 wandb.run.config["epochs"] = args.epochs
 wandb.run.config["accs"] = args.accs
 for i in range(wandb.run.config.epochs):
     wandb.log({"loss": 1000000/((i+1)*wandb.config.lr)})
-    if wandb.run.config.accs:
-        wandb.log({"acc": 1 - 2/(i+1)})
+    wandb.log({"acc": 1 - 2/(i+1)})
 
     
