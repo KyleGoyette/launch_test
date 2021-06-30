6baa5883a8c9068f0de8d3e72dfad36c02d1374c
Kyle Goyette <kdgoyette@gmail.com>
2021-06-30 14:26:27 -0700

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import wandb
 import argparse
-import numpy
+import math
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--lr", type=float, default=0.1)
@@ -8,13 +9,25 @@ parser.add_argument("--epochs", type=int, default=5)
 parser.add_argument("--seed", type=int, default=10)
 args = parser.parse_args()
 
-wandb.init(project="launch-demo")
-wandb.run.config["nested"] = {"super_nested": "jerk"}
-wandb.run.config["lr"] = args.lr
-wandb.run.config["epochs"] = args.epochs
-wandb.run.config["accs"] = args.accs
-for i in range(wandb.run.config.epochs):
-    wandb.log({"loss": 1000000/((i+1)*wandb.config.lr)})
-    wandb.log({"acc": 1 - 2/(i+1)})
+run = wandb.init(
+        project="launch-demo",
+        config={
+            "learning_rate": args.lr,
+            "batch_size": 128,
+            "momentum": 0.1,
+            "dropout": 0.2,
+            "architecture": "CNN",
+            "epochs": args.epochs
+            "seed": args.seed
+        })
 
-    
+random.seed(run.config.seed)
+displacement1 = random.random()
+displacement2 = random.random()
+for step in range(100):
+    wandb.log({
+        "acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate + random.random() + displacement1 + random.random() * run.config.momentum),
+        "val_acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement1),
+        "loss": 0.04 * (4 - math.log(1 + step + random.random()) + random.random() * run.config.momentum + random.random() + displacement2),
+        "val_loss": 0.04 * (5 - math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement2),
+    })
