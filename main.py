885129c14b3b7635f3ea105b372ab897b3c69ad6
Kyle Goyette <kdgoyette@gmail.com>
2021-08-13 12:14:33 -0700

    WIP

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import wandb
-# from wandb.sdk.launch.launch_add import launch_add
 import argparse
 import math
-import time
+import numpy as np
 import random
 
 
@@ -25,23 +24,24 @@ run = wandb.init(
             "seed": args.seed
         })
 
-random.seed(run.config.seed)
-artifact = wandb.Artifact('my-orig-dataset', type='dataset')
-artifact.add_file('my-dataset.txt')
-run.log_artifact(artifact)
-#dataset = run.use_artifact('my-dataset2:v0')
-#run.config.dataset = dataset
-#print(dataset._name)
-# if dataset._name == 'my-dataset2':
-#     const = 1
-# else:
-#     const = 0
+np.random.seed(run.config.seed)
 
-# for step in range(5):
-#     wandb.log({
-#         "acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate + random.random() + displacement1 + random.random() * run.config.momentum),
-#         "val_acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement1),
-#         "loss": 0.04 * (4 - math.log(1 + step + random.random()) + random.random() * run.config.momentum + random.random() + displacement2),
-#         "val_loss": 0.04 * (5 - math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement2),
-#         "train_step": step*25
-#     })
+dataset = run.use_artifact('launch-arti-demo/my-orig-dataset:latest')
+run.config.dataset = dataset
+
+if run.config.dataset.name == 'my-new-dataset:v0':
+    const = 0.5
+else:
+    const = 0
+
+displacement1 = np.random.uniform(0, 0.5) + const
+displacement2 = np.random.uniform(0.5, 1) - const
+
+for step in range(5):
+    wandb.log({
+        "acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate + random.random() + displacement1 + random.random() * run.config.momentum),
+        "val_acc": 0.04 * (math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement1),
+        "loss": 0.04 * (4 - math.log(1 + step + random.random()) + random.random() * run.config.momentum + random.random() + displacement2),
+        "val_loss": 0.04 * (5 - math.log(1 + step + random.random()) + random.random() * run.config.learning_rate - random.random() + displacement2),
+        "train_step": step*25
+    })
