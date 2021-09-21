950cb79bf473ae4059cbe5ecdebbefe4c3736eac
Kyle Goyette <kdgoyette@gmail.com>
2021-09-21 14:04:44 -0700

    WIP

--- main3.py
+++ main3.py
@@ -2,7 +2,7 @@ import wandb
 import time
 run = wandb.init(project="arti-test")
 #time.sleep(15)
-artifact = run.use_artifact('my-dataset2:latest')
+artifact = run.use_artifact('my-dataset2:v0')
 #run.use_artifact('arti-test/my-dataset2:latest')
 print(artifact.name)
 wandb.log({"stuff": 'asdf'})
