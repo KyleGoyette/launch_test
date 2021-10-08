ed42af6e85acc9b1995dea995a539d6c4fc7786e
Kyle Goyette <kdgoyette@gmail.com>
2021-10-07 20:39:56 -0700

    WIP

--- main3.py
+++ main3.py
@@ -2,15 +2,17 @@ import wandb
 import time
 run = wandb.init(project="arti-test")
 time.sleep(5)
-artifact = run.use_artifact('my-boom-arti:latest', use_as="fork")
-artifact = run.use_artifact('my-boom-arti:latest', use_as="fork2")
+artifact = run.use_artifact('my-new-arti:latest', use_as="fork1")
+artifact2 = run.use_artifact('my-new-arti:latest', use_as="fork2")
+artifact3 = run.use_artifact('my-new-arti:latest')
+artifact4 = run.use_artifact('my-new-arti:latest')
 #artifact = wandb.Artifact('my-boom-arti', type='model', use_as="blah")
 #run.use_artifact(artifact)
 #artifact.wait()
 print(artifact.version)
 run.config.update({"arti": {"dataset": artifact}})
 
-run.config.garbage = artifact
+run.config.garbage = artifact2
 
 print("ABJKDSA")
 print(run.config.arti["dataset"])
