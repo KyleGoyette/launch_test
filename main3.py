f978e59715283f8e26b2f5d336fe138bde85fbe5
Kyle Goyette <kdgoyette@gmail.com>
2021-10-20 14:42:35 -0700

    WIP

--- main3.py
+++ main3.py
@@ -1,5 +1,6 @@
 import wandb
 import random
+import time
 
 config_dict = {
     "lr": 0.01,
@@ -7,17 +8,17 @@ config_dict = {
     "epochs": 10,
 }
 run = wandb.init(project="launch-artifact-demo", config=config_dict)
-
+time.sleep(5)
 dataset = run.use_artifact("my-dataset:latest", use_as="dataset")
 model = run.use_artifact("my-bad-model:latest", use_as="model")
 
 print(f"Using model {model.name}")
 print(f"Using dataset: {dataset.name}")
 run.config.dataset = dataset
+if model.name == "my-good-model":
+    v1 = 1.0
+else:
+    v1 = 0.5
 for epoch in range(run.config.epochs):
-    if model.name == "my-good-model":
-        v1 = 1.0
-    else:
-        v1 = 0.5
-    wandb.log({"good_metric": epoch*(random.random() * run.config.lr*v1 + run.config.decay*1e6)})
-    wandb.log({"bad_metric": 1 - epoch*(random.random() * run.config.lr*v1 + run.config.decay*1e6)})
+    wandb.log({"good_metric": random.random()*10*run.config.lr*v1*epoch + run.config.decay*1e6})
+    wandb.log({"bad_metric": 1 - epoch*(random.random()*10*run.config.lr*v1*epoch + run.config.decay*1e6)})
