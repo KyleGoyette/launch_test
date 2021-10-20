6120cbb791cbefce666114a81d2d3ae62af8562c
Kyle Goyette <kdgoyette@gmail.com>
2021-10-20 14:53:38 -0700

    WIP

---
+++ main2.py
@@ -0,0 +1,15 @@
+import wandb
+import numpy as np
+import time
+
+run = wandb.init(project="launch-artifact-demo")
+
+artifact = wandb.Artifact('my-dataset', type='dataset')
+print(artifact.name)
+artifact.add_file('my-dataset.txt')
+run.log_artifact(artifact)
+artifact.wait()
+#print(artifact._sequence_name)
+run.config.dataset = artifact
+run.log_code()
+#wandb.log({"stuff": 'asdf'})
