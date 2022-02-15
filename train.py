872d0be9cbfd1e92b50f498d13bc2a51003d4a73
Kyle Goyette <kdgoyette@gmail.com>
2022-02-15 14:44:28 -0800

    wip

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ config_dict = {
     "lr": 0.01,
     "decay": 1e-6,
     "epochs": 100,
-    "model": "wandb-artifact://https://api.wandb.ai/kylegoyette/model_registry_ex/mnist_ds:v6"
+    #"model": "wandb-artifact://https://api.wandb.ai/kylegoyette/model_registry_ex/mnist_ds:v6"
 }
 
 
@@ -19,16 +19,17 @@ run = wandb.init(project="triggers-demo", config=config_dict)
 #run.config.model = "wandb-artifact://my-bad-model:latest"
 # run.config.dataset = "wandb-artifact://my-dataset:latest"
 
-print("fork", run.config.model)
-print(f"Using model {run.config.model.name}")
-print()
+# print("fork", run.config.model)
+# print(f"Using model {run.config.model.name}")
+# print()
 
 #run.config.model = model
 #run.config.dataset = dataset
-if "my-good-model" in run.config.model.name:
-    v1 = 2.0
-else:
-    v1 = 0.5
+# if "my-good-model" in run.config.model.name:
+#     v1 = 2.0
+# else:
+    # v1 = 0.5
+v1 = 1
 for epoch in range(run.config.epochs):
     wandb.log({"good_metric": random.random()*10*run.config.lr*v1*epoch + run.config.decay*1e6})
     wandb.log({"bad_metric": 1 - epoch*(random.random()*10*run.config.lr*v1*epoch + run.config.decay*1e6)})
